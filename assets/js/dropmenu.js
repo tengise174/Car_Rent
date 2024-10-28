6fc93b29284f581be6faf67e27adedac6777abfc
tengis <tengise174@gmail.com>
2024-10-29 00:57:33 +0800

    dropdown menu

---
+++ assets/js/dropmenu.js
@@ -0,0 +1,15 @@
+document.getElementById('user-dropdown-toggle').addEventListener('click', function (e) {
+    e.preventDefault();
+    const dropdown = document.getElementById('user-dropdown');
+    dropdown.classList.toggle('active');
+});
+
+// Optional: Close dropdown if clicked outside
+window.addEventListener('click', function (e) {
+    const dropdown = document.getElementById('user-dropdown');
+    if (!dropdown.contains(e.target) && !document.getElementById('user-dropdown-toggle').contains(e.target)) {
+        dropdown.classList.remove('active');
+    }
+});
+
+
